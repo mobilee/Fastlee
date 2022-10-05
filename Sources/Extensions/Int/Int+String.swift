497884e1388fd5d42c37666754c886304e6fcf0d
Łukasz Szarkowicz <szarkowicz@gmail.com>
2022-10-05 14:12:34 +0200

    Add extension Int to String

---
+++ Sources/Extensions/Int/Int+String.swift
@@ -0,0 +1,19 @@
+//
+//  Int+String.swift
+//  Fastlee
+//
+//  Created by Łukasz Szarkowicz on 05/10/2022.
+//  Copyright © 2022 Mobilee. All rights reserved.
+//
+
+import Foundation
+
+public extension Int {
+    
+    /**
+     Creates string from given Int
+     */
+    var string: String {
+        return "\(self)"
+    }
+}
